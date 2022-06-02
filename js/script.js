418f8f88c1da37ae36a2e13e718aa936c4bd921c
Masum Alam <masum.7788alam@gmail.com>
2022-06-02 11:27:19 +0530

    no chnange

---
+++ js/script.js
@@ -0,0 +1,24 @@
+myScroll.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        behavior:"smooth",
+    })
+
+
+}
+/*
+myBtn.onclick = () => {
+    window, scrollTo({
+        bottom: 0,
+        behavior:"smooth", 
+    
+    })
+
+}*/
+$("#hideNav").click(function () {
+
+    $("#openNav").slideToggle();
+
+
+})
+
